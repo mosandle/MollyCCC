fbef0a8b736e5ce50cc0c53246db79f029a11900
Molly Sandler <molly.sandler13@gmail.com>
2023-10-04 11:40:31 -0700

    catalog now lists all three colors

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -15,21 +15,44 @@ def get_catalog():
     #currently lists one single red potion no matter the quantity, if 0 in possession lists 0
     
     with db.engine.begin() as connection:
-        sql_statement = text("SELECT num_red_potions FROM global_inventory LIMIT 20")
+        sql_statement = text("SELECT num_red_potions, num_green_potions, num_blue_potions FROM global_inventory LIMIT 20")
         result = connection.execute(sql_statement)
         row = result.first()
-        if row[0] > 0:
+        num_red = row[0]
+        num_green = row[1]
+        num_blue = row[2]
+        if num_red > 0:
             return [
                 {
                     "sku": "RED_POTION_0",  # Basic sku
                     "name": "red potion",  # Basic name
-                    "quantity": 1,  # Will only ever list one red potion for $50 right now
-                    "price": 50,  # Price hardcoded at $50.
+                    "quantity": num_red,  #listing the actual amount of red potions I have
+                    "price": 55,  # Price hardcoded at $55.
                     "potion_type": [100, 0, 0, 0],  # Red potion rgb value
                 }
             ]
+        elif num_green > 0:
+            return [
+                {
+                    "sku": "GREEN_POTION_0",  # Basic sku
+                    "name": "green potion",  # Basic name
+                    "quantity": num_green,  #listing the actual amount of green potions I have
+                    "price": 55,  # Price hardcoded at $55.
+                    "potion_type": [0, 100, 0, 0],  # green potion rgb value
+                }
+            ]
+        elif num_blue > 0:
+            return [
+                {
+                    "sku": "BLUE_POTION_0",  # Basic sku
+                    "name": "blue potion",  # Basic name
+                    "quantity": num_blue,  #listing the actual amount of green potions I have
+                    "price": 55,  # Price hardcoded at $55.
+                    "potion_type": [0, 0, 100, 0],  # blue potion rgb value
+                }
+            ]
         else:
-            return []
+            return [] #no potions of any color available
 
 
 
